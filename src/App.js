e82761416dee5f7a8a98dcd59700bc42b037af31
emilbergs <emilberg@outlook.dk>
2020-11-04 21:12:33 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+
+    console.log(this.props.data)
+    return (
+      <ul>
+        <li className="boy">Name 1</li>
+        <li className="girl">Name 1</li>
+      </ul>
+    );
+  }
 }
 
 export default App;
